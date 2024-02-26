816c60c3205f4518eb4a0b66d4c2a57a6190bf34
mrlord3 <160180670+mrlord3@users.noreply.github.com>
2024-02-26 12:38:31 +0200

    Update userbot.py

--- ZeMusic/core/userbot.py
+++ ZeMusic/core/userbot.py
@@ -57,7 +57,7 @@ class Userbot(Client):
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك عفرتو")
+                await self.one.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك لورد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
